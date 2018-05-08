3eb2fae42d03655689fa8fa685441da3afb5d1b3
Stephan Krauss <info@stephankrauss.de>
2018-05-08 07:12:14 +0200

    erster Test mit Array

--- index.php
+++ index.php
@@ -4,14 +4,34 @@
 	use JsonSchema\Validator;
 	use JsonSchema\Constraints\Constraint;
 
-	$fileContent = file_get_contents('config.json');
-	$config = json_decode($fileContent);
+	$content = [
+		0 => [
+			'vorname' => 'Stephan',
+			'name' => 'Krauss',
+			'alter' => 60
+		],
+		[
+			'vorname' => 'Antje',
+			'name' => 'Krauss',
+			'alter' => 59
+		]
+	];
+
+	var_dump($content);
+	exit();
+
+	$jsonContent = json_encode($content);
+
+	// $fileContent = file_get_contents('config.json');
+	// $config = json_decode($fileContent);
+
+	$config = json_decode($jsonContent);
 
 	$validator = new Validator;
 
 	$validator->validate(
 	  $config,
-	  (object)['$ref' => 'file://' . realpath('schema.json')],
+	  (object)['$ref' => 'file://' . realpath('array_schema.json')],
 	  Constraint::CHECK_MODE_APPLY_DEFAULTS
 	);
 
